891f8b9d166805008aac47cd5c8ab557941613df
brycecoder37 <73134123+brycecoder37@users.noreply.github.com>
2022-02-20 22:27:23 -0700

    Created a test class to practice running tests

--- src/test/java/OrientationTest.java
+++ src/test/java/OrientationTest.java
@@ -1,11 +1,15 @@
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class OrientationTest {
 
     public OrientationTest() {
     }
 
     @Test
-    public void test1() {}
+    public void test1() {
+        assertEquals(1,1);
+    }
 
 }
