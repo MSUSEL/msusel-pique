7a61014312321f6ce108680b1b21a2c231c4e4e8
Ryan Cummings <ryan.cummings1@montana.edu>
2024-10-15 10:54:46 -0600

    This provides a way to build test suites. Annotations can help define specific tests

---
+++ src/test/java/ExperimentalTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class ExperimentalTest {
+    // Fixme replace with real tests
+    @Test
+    public void testRegressionSuiteAnnotation() {
+        System.out.println("Regression suite running");
+        assert true;
+    }
+}
